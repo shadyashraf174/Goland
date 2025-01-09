1a6bd16bca97233fa4696c02420b522d2b9b82f8
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-01-09 23:13:36 +0700

    Other data structures Maps and Slices

--- main.go
+++ main.go
@@ -1,24 +1,46 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
-type myStruct struct {
+type User struct {
 	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
+	LastName  string
 }
 
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "shady"
 
-	myVar2 := myStruct{
-		FirstName: "Ashraf",
+	myMap := make(map[string]User)
+
+	me := User{
+		FirstName: "shady",
+		LastName:  "AShraf",
 	}
 
-	log.Println(myVar.printFirstName())
-	log.Println(myVar2.printFirstName())
+	myMap["me"] = me
 
+	log.Println(myMap["me"].FirstName)
+
+	// Slices
+
+	var mySlice []int
+
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 3)
+	mySlice = append(mySlice, 1)
+
+	sort.Ints(mySlice)
+
+	log.Println(mySlice)
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(numbers)
+	log.Println(numbers[0:2])
+	log.Println(numbers[6:8])
+
+	names := []string{"shady", "Ashraf", "Ahmed"}
+	log.Println(names)
+	log.Println(names[0:2])
 }
