d98f6197cbf8d699dcd501de332dc62b174bc831
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-01-12 15:41:24 +0700

    Decision Structures

--- main.go
+++ main.go
@@ -2,45 +2,39 @@ package main
 
 import (
 	"log"
-	"sort"
+	"net/http"
 )
-
-type User struct {
-	FirstName string
-	LastName  string
-}
 
 func main() {
 
-	myMap := make(map[string]User)
+	// var isTrue bool
+	// isTrue = false
 
-	me := User{
-		FirstName: "shady",
-		LastName:  "AShraf",
+	// if isTrue == true {
+	// 	log.Println(isTrue)
+	// } else {
+	// 	log.Println("nothing")
+	// }
+
+	cat := "cat"
+
+	// if cat == "cat" {
+	// 	log.Println("isTrue")
+	// } else {
+	// 	log.Println("isNotTrue")
+	// }
+
+	switch cat {
+	case "cat":
+		log.Println("cat is set to cat")
+
+	case "dog":
+		log.Println("cat is set to dog")
+
+	default:
+		log.Println("cat is set to somthing else")
 	}
 
-	myMap["me"] = me
-
-	log.Println(myMap["me"].FirstName)
-
-	// Slices
-
-	var mySlice []int
-
-	mySlice = append(mySlice, 2)
-	mySlice = append(mySlice, 3)
-	mySlice = append(mySlice, 1)
-
-	sort.Ints(mySlice)
-
-	log.Println(mySlice)
-
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	log.Println(numbers)
-	log.Println(numbers[0:2])
-	log.Println(numbers[6:8])
-
-	names := []string{"shady", "Ashraf", "Ahmed"}
-	log.Println(names)
-	log.Println(names[0:2])
 }
+
+//01:42:10
