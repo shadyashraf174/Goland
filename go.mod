a1f848631ab1b3d6db1ae17642af1f6d284b8b94
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-01-13 16:00:41 +0700

    Packages

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shadyashraf174/myniceprogram
+
+go 1.23.4
--- main.go
+++ main.go
@@ -1,58 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
 
-type Animal interface {
-	Says() string
-	NumberOfLegs() int
-}
-
-type Dog struct {
-	Name  string
-	Breed string
-}
-
-type Gorilla struct {
-	Name          string
-	Color         string
-	NumberOfTeeth int
-}
+	"github.com/shadyashraf174/myniceprogram/helpers"
+)
 
 func main() {
+	log.Println("Hello")
 
-	dog := Dog{
-		Name:  "Samson",
-		Breed: "shady",
-	}
+	var myvar helpers.SomeType
 
-	PrintInfo(dog)
-
-	gorilla := Gorilla{
-		Name:          "King Kong",
-		Color:         "Black",
-		NumberOfTeeth: 32,
-	}
-
-	PrintInfo(gorilla)
-
+	myvar.TypeName = "Shady"
+	log.Println(myvar.TypeName)
 }
 
-func (d Dog) Says() string {
-	return "woof"
-}
-
-func (d Dog) NumberOfLegs() int {
-	return 4
-}
-
-func (d Gorilla) Says() string {
-	return "grunt"
-}
-
-func (d Gorilla) NumberOfLegs() int {
-	return 2
-}
-
-func PrintInfo(a Animal) {
-	log.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "Legs")
-}
+// 02:04:35
