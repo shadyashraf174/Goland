4229000d33c5195db3ddce1ec7ee75f2132a4bd6
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-02-08 18:32:45 +0700

    something for me

--- main.go
+++ main.go
@@ -1,31 +1,70 @@
 package main
 
-import (
-	"log"
+import "fmt"
 
-	"github.com/shadyashraf174/myniceprogram/helpers"
+const (
+	version = 1
+	keylen  = 10
 )
 
-const numberPool = 1000
+type Player struct {
+	name        string
+	health      int
+	attackPower float64
+}
 
-func CalculateValue(intChan chan int) {
-	randomNumber := helpers.RandomNumber(numberPool)
-	intChan <- randomNumber
+var x = 10
+
+var (
+	floatVar32 float32 = 1.0
+	floatVar64 float64 = 0.1
+	name               = "foo"
+	firstName  string  = "foo"
+	lastName   string
+)
+
+func (player Player) getHealth() int {
+	return player.health
+}
+
+type Weapon string
+
+func getweapon(weapon Weapon) string {
+	return string(weapon)
 }
 
 func main() {
-	// pln("Hi")
 
-	intChan := make(chan int)
-	defer close(intChan)
+	numbers := []int{1, 2, 3, 4}
+	// users := map[string]int{}
+	// users := map[string]int{
+	// 	"shady": 34,
+	// }
 
-	go CalculateValue(intChan)
+	numbers = append(numbers, 5)
+	users := make(map[string]int)
 
-	num := <-intChan
-	log.Println(num)
+	users["foo"] = 10
+	users["bar"] = 30
 
+	name := 19
+	fmt.Println(x)
+
+	fmt.Println(name)
+	fmt.Println(version)
+
+	// Player := Player{
+	// 	name:        "captin jack",
+	// 	health:      100,
+	// 	attackPower: 45.1,
+	// }
+
+	age, ok := users["baz"]
+	if !ok {
+		fmt.Println("baz not exist")
+	} else {
+		fmt.Println("exist in the map: ", age)
+	}
+
+	fmt.Println(numbers)
 }
-
-// func pln(s string) {
-// 	log.Println(s)
-// }
