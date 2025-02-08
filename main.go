367eeabcc03f962a32e8e5b74a04e143c35886de
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-02-08 18:53:21 +0700

    this was about enums

--- main.go
+++ main.go
@@ -2,69 +2,40 @@ package main
 
 import "fmt"
 
+type WeaponType int
+
 const (
-	version = 1
-	keylen  = 10
+	Axe WeaponType = iota // increment
+	Sword
+	Woodenstick
+	Knife
 )
 
-type Player struct {
-	name        string
-	health      int
-	attackPower float64
+func (w WeaponType) String() string {
+	switch w {
+	case Sword:
+		return "SWORD"
+	case Axe:
+		return "AXE"
+	}
+	return ""
 }
 
-var x = 10
-
-var (
-	floatVar32 float32 = 1.0
-	floatVar64 float64 = 0.1
-	name               = "foo"
-	firstName  string  = "foo"
-	lastName   string
-)
-
-func (player Player) getHealth() int {
-	return player.health
-}
-
-type Weapon string
-
-func getweapon(weapon Weapon) string {
-	return string(weapon)
+func getDomage(weaponType WeaponType) int {
+	switch weaponType {
+	case Axe:
+		return 100
+	case Sword:
+		return 70
+	case Woodenstick:
+		return 20
+	case Knife:
+		return 60
+	default:
+		panic("Weapon does not exist")
+	}
 }
 
 func main() {
-
-	numbers := []int{1, 2, 3, 4}
-	// users := map[string]int{}
-	// users := map[string]int{
-	// 	"shady": 34,
-	// }
-
-	numbers = append(numbers, 5)
-	users := make(map[string]int)
-
-	users["foo"] = 10
-	users["bar"] = 30
-
-	name := 19
-	fmt.Println(x)
-
-	fmt.Println(name)
-	fmt.Println(version)
-
-	// Player := Player{
-	// 	name:        "captin jack",
-	// 	health:      100,
-	// 	attackPower: 45.1,
-	// }
-
-	age, ok := users["baz"]
-	if !ok {
-		fmt.Println("baz not exist")
-	} else {
-		fmt.Println("exist in the map: ", age)
-	}
-
-	fmt.Println(numbers)
+	fmt.Printf("damage of weapon (%s) (%d):\n", Axe, getDomage(Axe))
 }
