5a447a041a250f8aa112dcb79fb7c552a4f466f3
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-02-09 01:02:09 +0700

    models

--- main.go
+++ main.go
@@ -1,38 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-//stor(er)
-
-type NumberStorer interface {
-	GetAll() ([]int, error)
-	Put(int) error
-}
-
-type MongoDBNumberStore struct {
-	// some value here
-}
-
-func (m MongoDBNumberStore) GetAll() ([]int, error) {
-	return []int{1, 2, 3}, nil
-}
-
-func (m MongoDBNumberStore) Put(Number int) error {
-	fmt.Println("store the number into the mongoDB storage")
-	return nil
-}
-
-type Apiserver struct {
-	numberStore NumberStorer
-}
+	"github.com/shadyashraf174/myniceprogram/helpers"
+)
 
 func main() {
-	apiserver := Apiserver{
-		numberStore: MongoDBNumberStore{},
-	}
-	numbers, err := apiserver.numberStore.GetAll()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(numbers)
+	fmt.Println(helpers.RandomNumber(100000))
 }
