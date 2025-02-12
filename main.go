6ca9fb7d9a2657d542f1dedc83924d59ed402df9
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2025-02-12 11:23:22 +0700

    Advanced type techniques

--- main.go
+++ main.go
@@ -1,11 +1,66 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/shadyashraf174/myniceprogram/helpers"
+type Entity struct {
+	name    string
+	id      string
+	version string
+	posx    int
+	posy    int
+}
+
+type SpecialField struct {
+	Entity
+	SpecialField float64
+}
+
+type Color int
+
+func (c Color) String() string {
+	switch c {
+	case CorlorBlue:
+		return "BLUE"
+	case ColorBlack:
+		return "BLACK"
+	case ColorYellow:
+		return "YELLOW"
+	case ColorPink:
+		return "Pink"
+	default:
+		panic("invalid color given")
+	}
+}
+
+const (
+	CorlorBlue Color = iota + 1
+	ColorBlack
+	ColorYellow
+	ColorPink
 )
 
 func main() {
-	fmt.Println(helpers.RandomNumber(100000))
+
+	e := Entity{
+		name:    "shady",
+		id:      "id 1",
+		version: "version 1",
+		posx:    100,
+		posy:    200,
+	}
+
+	en := SpecialField{
+		SpecialField: 1.23,
+		Entity: Entity{
+			name:    "shady",
+			id:      "id 1",
+			version: "version 1",
+			posx:    100,
+			posy:    200,
+		},
+	}
+
+	fmt.Printf("this is the Entity%+v%+v\n", en.id, e)
+
+	fmt.Println("this color is: ", ColorBlack, CorlorBlue)
 }
